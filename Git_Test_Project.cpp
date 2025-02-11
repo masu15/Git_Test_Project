186f92791e36e20207d656dbc118dfed87c6f550
ます <k024g1021@g.neec.ac.jp>
2025-02-11 13:52:35 +0900

    Revert "こんにちは"

--- Git_Test_Project.cpp
+++ Git_Test_Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "PullRequest branch changed\n";
+    std::cout << "Hello New branch!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
